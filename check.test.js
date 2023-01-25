908b7ab722f74d610ec3d89e444899a48c77f04e
Ricky Medina <rickmedina85@gmail.com>
2023-01-25 14:30:32 -0400

    removed node:assert

--- check.test.js
+++ check.test.js
@@ -1,91 +1,90 @@
-import { describe, it } from "vitest"
-import { check, CONSTANTS as C } from "./check.js"
-import { deepEqual as assert_Eq, equal as assert_Is } from "node:assert/strict"
+import { describe, it, expect as expect_ } from "vitest"
+import { check_Eq, check_not_Eq, check_is, check_isNot, CONSTANTS as C } from "./check.js"
 
 function _assertCheckErr(err, rec, exp, opID, opMsg) {
 
 	const errKs = new Set(Object.getOwnPropertyNames(err))
 	const expKs = new Set(["type", "op", "message", "received", "expected", "stack"])
-	assert_Eq(errKs, expKs)
-	assert_Eq(err.type, C.FAIL)
-	assert_Eq(typeof err.stack, "string")
+	expect_(errKs).toEqual(expKs)
+	expect_(err.type).toEqual(C.FAIL)
+	expect_(typeof err.stack).toEqual("string")
 
-	assert_Eq(err.received, rec)
-	assert_Is(err.received, rec)
-	assert_Eq(err.expected, exp)
-	assert_Is(err.expected, exp)
+	expect_(err.received).toEqual(rec)
+	expect_(err.received).toBe(rec)
+	expect_(err.expected).toEqual(exp)
+	expect_(err.expected).toBe(exp)
 
-	assert_Eq(err.op, opID)
-	assert_Eq(err.message, opMsg)
+	expect_(err.op).toEqual(opID)
+	expect_(err.message).toEqual(opMsg)
 }
 
-describe("equal()", () => {
+describe("check_Eq()", () => {
 
-	it.only("should throw correct object", () => {
+	it("should throw correct object", () => {
 		const exp = ["exp"]
 		const rec = ["rec"]
 		try {
-			check(rec).equal(exp)
+			check_Eq(rec, exp)
 		} catch (err) {
-			_assertCheckErr(err, rec, exp, C.OP_EQ_ID, C.OP_EQ_MSG)
+			_assertCheckErr(err, rec, exp, C.OP_EQ, C.OP_EQ_MSG)
 		}
 	})
 
 	it("should not throw", () => {
-		check(["exp"]).equal(["exp"])
+		check_Eq(["exp"], ["exp"])
 	})
 })
 
-describe("not equal()", () => {
+describe("check_not_Eq()", () => {
 
 	it("should throw correct object", () => {
 		const rec = ["x"]
 		const exp = ["x"]
 		try {
-			check(rec).not.equal(exp)
+			check_not_Eq(rec, exp)
 		} catch (err) {
-			_assertCheckErr(err, rec, exp, C.OP_NOTEQ_ID, C.OP_NOTEQ_MSG)
+			_assertCheckErr(err, rec, exp, C.OP_NOT_EQ, C.OP_NOT_EQ_MSG)
 		}
 	})
 
 	it("should not throw", () => {
-		check(["rec"]).not.equal(["exp"])
+		check_not_Eq(["rec"], ["exp"])
 	})
 })
 
-describe("is()", () => {
+describe("check_is()", () => {
 
 	it("should throw correct object", () => {
 		const exp = {}
 		const rec = {}
 		try {
-			check(rec).is(exp)
+			check_is(rec, exp)
 		} catch (err) {
-			_assertCheckErr(err, rec, exp, C.OP_IS_ID, C.OP_IS_MSG)
+			_assertCheckErr(err, rec, exp, C.OP_IS, C.OP_IS_MSG)
 		}
 	})
 
 	it("should not throw", () => {
 		let obj = {}
 		obj.a = 1
-		check(obj).is(obj)
+		check_is(obj, obj)
 	})
 })
 
-describe("not is()", () => {
+describe("check_isNot()", () => {
 
 	it("should throw correct object", () => {
 		let rec = {}
 		rec.a = 1
 		let exp = rec
 		try {
-			check(rec).not.is(rec)
+			check_isNot(exp, rec)
 		} catch (err) {
-			_assertCheckErr(err, rec, exp, C.OP_ISNOT_ID, C.OP_ISNOT_MSG)
+			_assertCheckErr(err, rec, exp, C.OP_NOT_IS, C.OP_NOT_IS_MSG)
 		}
 	})
 
 	it("should not throw", () => {
-		check(["rec"]).not.is(["exp"])
+		check_isNot(["rec"], ["exp"])
 	})
 })
